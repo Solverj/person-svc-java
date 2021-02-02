b48b6ad765d1c71164aea2208c5537cf84aed7f0
Sølve Stålem <solve.stalem@bisnode.com>
2021-02-02 17:50:17 +0100

    simple clean lookup statsup.

--- src/main/java/no/solve/personsvc/PersonController.java
+++ src/main/java/no/solve/personsvc/PersonController.java
@@ -1,28 +1,13 @@
 package no.solve.personsvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController("/")
 public class PersonController {
 
-    private final PersonRepository personRepository;
-
-    @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
-
-    @GetMapping("/persons")
-    public List<Person> getPersons(){
-        return personRepository.getPersons();
-    }
-
     @GetMapping(path = "/")
-    public String getStatus(){
+    public String getStatus() {
         return "{\"status\": \"UP\"}";
     }
 }
